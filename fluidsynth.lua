cba651bb2e617fc4e3a293aaaa7c7076bd170a72
David Konsumer <konsumer@jetboystudio.com>
2022-05-25 20:47:15 -0700

    inst management

--- fluidsynth.lua
+++ fluidsynth.lua
@@ -6,24 +6,44 @@ local fluidsynth = {}
 
 -- TODO: do midi splits: https://www.fluidsynth.org/api/MIDIRouter.html
 
-local tcp
+local tcp = assert(socket.tcp())
 
-function fluidsynth:init()
+-- start fluidsynth in another thread
+function fluidsynth:init(file)
   local t = love.thread.newThread( [[
-    local command = ...
-    os.execute(command)
+    local file = ...
+    if file then
+      os.execute(string.format('fluidsynth -q -s -g 1 -d -p lovesynth -o "midi.autoconnect=1" -o "shell.port=31337" %q', file))
+    else
+      os.execute('fluidsynth -q -s -g 1 -d -p lovesynth -o "midi.autoconnect=1" -o "shell.port=31337"')
+    end
   ]])
-  t:start('fluidsynth -q -s -g 1 -d -p lovesynth -o "midi.autoconnect=1" -o "shell.port=31337" ')
-  tcp = assert(socket.tcp())
+  t:start(file)
 end
 
+-- send a single command over tcp
 function fluidsynth:cmd(c)
-  print('COMMAND: ' .. c)
   tcp:connect('localhost', 31337)
   tcp:send(c .. '\n')
   tcp:close()
 end
 
+-- send a noteon message
+function fluidsynth:noteon(channel, note, velocity)
+  fluidsynth:cmd(string.format('noteon %d %d %d', channel, note, velocity))
+end
+
+-- send a noteoff message
+function fluidsynth:noteoff(channel, note)
+  fluidsynth:cmd(string.format('noteoff %d %d', channel, note))
+end
+
+-- send a load message
+function fluidsynth:load(file)
+  fluidsynth:cmd(string.format('load %q', file))
+end
+
+-- close tcp & kill all copies of fluidsynth
 function fluidsynth:close()
   tcp:close()
   io.popen('killall -9 fluidsynth')
--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 require("input")
 local fluidsynth = require('fluidsynth')
 
-fluidsynth:init()
-love.timer.sleep(1)
-fluidsynth:cmd('load ./sf2/Synensmb.sf2')
+local files = love.filesystem.getDirectoryItems('sf2')
+local currentFile = 1
+
+fluidsynth:init(string.format('sf2/%s', files[currentFile]))
 
 function love.load(args)
 end
@@ -12,23 +13,47 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.printf(files[currentFile], 10, 10, 460, 'center')
+end
+
+function input_pressed(button)
+  local change = false
+  if button == 'a' then
+    fluidsynth:noteon(currentFile, 60, 127)
+  end
+  if button == 'b' then
+    fluidsynth:noteon(currentFile, 62, 127)
+  end
+
+  if button == 'up' then
+    currentFile = currentFile - 1
+    change = true
+  end
+
+  if button == 'down' then
+    currentFile = currentFile + 1
+    change = true
+  end
+
+  if currentFile > #files then
+    currentFile = 1
+  end
+  if currentFile < 1 then
+    currentFile = #files
+  end
+
+  if change then
+    fluidsynth:load(string.format('sf2/%s', files[currentFile]))
+    fluidsynth:cmd(string.format('inst %d', currentFile))
+  end
 end
 
 function input_released(button)
   if button == 'a' then
-    fluidsynth:cmd('noteoff 1 60')
+    fluidsynth:noteoff(currentFile, 60)
   end
   if button == 'b' then
-    fluidsynth:cmd('noteoff 1 62')
-  end
-end
-
-function input_pressed(button)
-  if button == 'a' then
-    fluidsynth:cmd('noteon 1 60 127')
-  end
-  if button == 'b' then
-    fluidsynth:cmd('noteon 1 62 127')
+    fluidsynth:noteoff(currentFile, 62)
   end
 end
 
